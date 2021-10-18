d226d123b1837cac1fea760cba4bdb22a4a1d00d
dail45 <69967573+dail45@users.noreply.github.com>
2021-10-18 14:44:21 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 import requests
 import os
 import urllib3
+import lzma
 
 app = Flask(__name__)
 
@@ -64,7 +65,7 @@ def generage_download_file_chunks():
     while True:
         if len(file_chunks) < (64*1024*1024 // chunk_size) + 2:
             try:
-                file_chunks[file_chunks["counter"] + 1] = next(generator)
+                file_chunks[file_chunks["counter"] + 1] = lzma.compress(next(generator))
                 file_chunks["counter"] += 1
             except StopIteration:
                 break
@@ -77,6 +78,14 @@ def await_chunk(count):
     if a == -1:
         return "0"
     return f"{a}"
+
+
+@app.route("/chunkinfo/<int:count>")
+def chunkinfo(count):
+    global file_chunks
+    res = file_chunks[count]
+    data = {"num": count, "len": len(res)}
+    return data
 
 
 @app.route("/download_chunk/<int:count>")
