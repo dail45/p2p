8b0bf7e8fa2fd23229699a9b3a1b8e90a1af645e
dail45 <69967573+dail45@users.noreply.github.com>
2021-10-21 15:58:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,128 +1,155 @@
+import os
 import time
+import string
+import random
+import requests
 import threading
 from flask import Flask, request
-import requests
-import os
-import urllib3
+
 
 app = Flask(__name__)
 
 
-def generator_chunks_number():
-    counter = 0
-    end = int(total_length) // (chunk_size) + 1
-    while counter < end:
-        if counter < file_chunks["counter"]:
-            counter += 1
-            print(file_chunks.keys(), file_chunks["counter"])
-            yield counter
-        else:
-            yield -1
-
-
-download_link = ""
-file_chunks = {"counter": 0, "counter_upload": 0}
-total_length = 0
-chunk_size = 4*1024*1024
-THREADS = 16
-file_chunks_number_gen = None
-
-
 @app.route("/")
-def fun():
-    return "Ok."
+def about():
+    return "p2p-tunnel v2"
 
 
-@app.route("/download")
-def download():
-    global download_link, total_length, file_chunks_number_gen, chunk_size, THREADS
-    if download_status() == "alive":
-        return "0"
-    restart()
-    download_link = request.args["public_key"]
-    if "Chunk-Size" in request.args:
-        chunk_size = int(request.args["Chunk-Size"])
-    else:
-        chunk_size = 4*1024*1024
-    if "Threads" in request.args:
-        THREADS = int(request.args["Threads"])
-    else:
-        THREADS = 16
-    data = requests.get(download_link, stream=True)
-    ret = data.headers
-    total_length = int(ret["Content-Length"])
-    data.close()
-    a = threading.Thread(target=generage_download_file_chunks)
-    a.start()
-    file_chunks_number_gen = generator_chunks_number()
-    return str(ret)
-
-
-def generage_download_file_chunks():
-    global file_chunks
-    #http = urllib3.PoolManager()
-    #r = http.urlopen("GET", download_link, preload_content=False, verify=False)
-    #r.auto_close = False
-    time.sleep(5)
-    r = requests.get(download_link, verify=False, stream=True)
-    generator = r.iter_content(chunk_size)
-    while True:
-        if len(file_chunks) < ((THREADS * chunk_size) // chunk_size) + 2:
-            try:
-                file_chunks[file_chunks["counter"] + 1] = next(generator)
-                file_chunks["counter"] += 1
-            except StopIteration:
-                break
-
-
-@app.route("/await_chunk/<int:count>")
-def await_chunk(count):
-    global file_chunks
-    a = next(file_chunks_number_gen)
-    if a == -1:
-        return "0"
-    return f"{a}"
-
-
-@app.route("/download_chunk/<int:count>")
-def download_chunk(count):
-    global file_chunks
-    res = file_chunks[count]
-    del file_chunks[count]
-    file_chunks["counter_upload"] += 1
-    return res
-
-
-@app.route("/downloadStatus")
-def download_status():
-    num = total_length // chunk_size
-    if num > 0:
-        num += 1
-    if file_chunks["counter_upload"] < num:
-        return "alive"
-    return "dead"
-
-
-@app.route("/restart")
-def restart():
-    global download_link, file_chunks, total_length, file_chunks_number_gen
-    download_link = ""
-    file_chunks = {"counter": 0, "counter_upload": 0}
+class P2PTunnel:
     total_length = 0
-    file_chunks_number_gen = None
-    chunk_size = 4 * 1024 * 1024
-    return "restart done"
+    STORAGE = {}
+    DOWNLOADED = 0
+    UPLOADED = 0
+
+    def __init__(self):
+        pass
+
+    def init(self, id, CHUNKSIZE=2**20*4, THREADS=16, RAM=2**20*64, URL=None):
+        self.id = id
+        self.URL = URL
+        self.CHUNKSIZE = CHUNKSIZE
+        self.THREADS = THREADS
+        self.RAM = RAM
+        self.numgeneratorg = self.numgenerator()
+        self.statuskillflag = True
+        if self.URL:
+            self.req = requests.get(URL, verify=False, stream=True)
+            headers = self.req.headers
+            self.r = self.req.iter_content(self.CHUNKSIZE)
+            self.total_length = int(headers["Content-Length"])
+            self.th = threading.Thread(target=self.S2Pdownloadgenerator)
+            self.th.start()
+            return headers
+        return {"id": self.id, "URL": self.URL, "chunksize": self.CHUNKSIZE, "threads": self.THREADS, "RAM": self.RAM}
+
+    def numgenerator(self):
+        counter = 0
+        end = int(self.total_length) // self.CHUNKSIZE + 1
+        while counter < end:
+            if counter < self.DOWNLOADED:
+                counter += 1
+                yield counter
+            else:
+                yield -1
+
+    def S2Pdownloadgenerator(self):
+        if self.URL:
+            while len(self.STORAGE) < self.RAM * self.CHUNKSIZE:
+                try:
+                    self.STORAGE[self.DOWNLOADED + 1] = next(self.r)
+                    self.DOWNLOADED += 1
+                except StopIteration:
+                    break
+
+    def uploadstatus(self):
+        num = self.total_length // self.CHUNKSIZE
+        if num > 0:
+            num += 1
+        if self.UPLOADED < num:
+            if len(self.STORAGE) == 0 and not self.th.is_alive():
+                self.th = threading.Thread(target=self.S2Pdownloadgenerator)
+                self.th.start()
+            return "alive"
+        return "dead"
+
+    def upload_await(self):
+        a = next(self.numgeneratorg)
+        if a == -1:
+            return "0"
+        return f"{a}"
+
+    def upload(self, count):
+        res = self.STORAGE[count]
+        del self.STORAGE[count]
+        self.UPLOADED += 1
+        return res
 
 
-@app.route("/log")
-def log():
-    return str({"download_link": download_link,
-            "file_chunks": file_chunks.keys(),
-            "file_chunks['counter']": file_chunks["counter"],
-            "total_length": total_length,
-            "file_chunks_number_gen": file_chunks_number_gen})
+rnums = {}
+@app.route("/reg")
+def registration():
+    nums = list(map(str, range(10)))
+    rnum = int("".join(random.sample(nums, 4)))
+    while rnum in rnums:
+        rnum = int("".join(random.sample(nums, 4)))
+
+    rnums[rnum] = P2PTunnel()
+    return str(rnum)
 
 
+@app.route("/kill/<int:rnum>")
+def kill(rnum):
+    del rnums[rnum]
+
+
+@app.route("/start/<int:rnum>")
+def start(rnum):
+    id = rnum
+    CHUNKSIZE = 2 ** 20 * 4
+    THREADS = 16
+    RAM = 2 ** 20 * 64
+    URL = None
+    args = request.args
+    if "CHUNSKSIZE" in args:
+        CHUNKSIZE = int(args["CHUNKSIZE"])
+    if "THREADS" in args:
+        THREADS = int(args["THREADS"])
+    if "RAM" in args:
+        RAM = int(args["RAM"])
+    if "URL" in args:
+        URL = args["URL"]
+    res = rnums[id].init(id, CHUNKSIZE, THREADS, RAM, URL)
+    return str(res)
+
+
+@app.route("/downloadStatus/<int:rnum>")
+def download_status(rnum):
+    log.append(f"{time.strftime('%H:%M:%S', time.gmtime(time.time()))}: downloadstatus<br>")
+    status = rnums[rnum].uploadstatus()
+    if status == "dead" and rnums[rnum].statuskillflag:
+        rnums[rnum].statuskillflag = False
+        time.sleep(5)
+        kill(rnum)
+    return status
+
+
+@app.route("/awaitChunk/<int:rnum>")
+def await_chunk(rnum):
+    log.append(f"{time.strftime('%H:%M:%S', time.gmtime(time.time()))}: await<br>")
+    return rnums[rnum].upload_await()
+
+
+@app.route("/downloadChunk/<int:rnum>/<int:count>")
+def download_chunk(rnum, count):
+    log.append(f"{time.strftime('%H:%M:%S', time.gmtime(time.time()))}: chunk<br>")
+    return rnums[rnum].upload(count)
+
+
+@app.route("/get")
+def logfunc():
+    return str(log)
+
+log = []
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
