91493c40a21bcb76e80e1ec6335c109cb9ee8cb2
dail45 <69967573+dail45@users.noreply.github.com>
2022-04-14 21:18:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-import socket, os
+import socket, os, flask
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", int(os.environ.get("PORT", 8000))))
-sock.listen(16)
-conn, addr = sock.accept()
-print(int(os.environ.get("PORT", 8000)))
-while True:
-    ln = int.from_bytes(conn.recv(8), "big")
-    if ln == 0:
-        continue
-    msg = conn.recv(ln)
-    print(f"msg: {msg}")
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def about():
+    return str(os.environ.get("PORT", 8000))
+
+
+@app.route("/cs")
+def create_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("", int(os.environ.get("PORT", 8000))))
+    sock.listen(16)
+    conn, addr = sock.accept()
+    while True:
+        ln = int.from_bytes(conn.recv(8), "big")
+        if ln == 0:
+            continue
+        msg = conn.recv(ln)
+        print(f"msg: {msg}")
+
+
+app.run("0.0.0.0", port=int(os.environ.get("PORT", 8000)))
