123f35a8f958f663f7322cea9dce3d77f1a201b7
dail45 <69967573+dail45@users.noreply.github.com>
2022-04-14 21:14:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("", int(os.environ.get("PORT", 8000))))
 sock.listen(16)
 conn, addr = sock.accept()
+print(int(os.environ.get("PORT", 8000)))
 while True:
-    ln = conn.recv(8)
-    print(f"msg len: {int.from_bytes(ln, 'big')}")
-    msg = conn.recv(int.from_bytes(ln, "big"))
+    ln = int.from_bytes(conn.recv(8), "big")
+    if ln == 0:
+        continue
+    msg = conn.recv(ln)
     print(f"msg: {msg}")
